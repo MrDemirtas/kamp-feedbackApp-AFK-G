b0cf410118b27c011a838257fe1c8aa04bd04786
Furkan Demirtaş <furkan-demirtas@outlook.com>
2025-02-13 10:55:25 +0300

    fetch işlemi yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/data/feedback-data.json")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
 
   return (
     <>
