03178e45551ad6133a407dd2b068297815cebeed
Furkan Demirtaş <furkan-demirtas@outlook.com>
2025-02-16 14:04:57 +0300

    iyileştirmeler yapıldı

--- src/components/FeedbackDetail.jsx
+++ src/components/FeedbackDetail.jsx
@@ -1,4 +1,5 @@
 import "../css/feedbackdetail.css";
+
 import { Data, Route, ScreenSize } from "../App";
 import { Fragment, useContext, useEffect, useState } from "react";
 
@@ -6,22 +7,22 @@ export default function FeedbackDetail() {
   const { data, setData } = useContext(Data);
   const screenSize = useContext(ScreenSize);
   const route = useContext(Route);
-  const [newComment, setNewComment] = useState('');
-  const [reply, setReply] = useState('');
-  const [replyContent, setReplyContent] = useState('');
-  const [currentFeedback, setCurrentFeedback] = useState(data.feedbacks.find(feedback => feedback.id === location.hash.substring(1).split('/').at(-1)));
+  const [newComment, setNewComment] = useState("");
+  const [reply, setReply] = useState("");
+  const [replyContent, setReplyContent] = useState("");
+  const [currentFeedback, setCurrentFeedback] = useState(data.feedbacks.find((feedback) => feedback.id === location.hash.substring(1).split("/").at(-1)));
 
   useEffect(() => {
-    setCurrentFeedback(data.feedbacks.find(feedback => feedback.id === location.hash.substring(1).split('/').at(-1)));
+    setCurrentFeedback(data.feedbacks.find((feedback) => feedback.id === location.hash.substring(1).split("/").at(-1)));
   }, [route]);
 
   if (!currentFeedback) {
-    location.hash = '/404';
+    location.hash = "/404";
     return;
   }
 
   useEffect(() => {
-    setReplyContent('');
+    setReplyContent("");
   }, [reply]);
 
   function handleSubmit(e) {
@@ -33,14 +34,14 @@ export default function FeedbackDetail() {
       username: data.currentUser.username,
       content: newComment,
       image: data.currentUser.image,
-      replies: []
-    }
+      replies: [],
+    };
     currentFeedback.comments.push(userComment);
     setData({ ...data });
-    setNewComment('');
+    setNewComment("");
   }
 
-  function handleReplySubmit(e, replyToComment = false, replyUserName = '') {
+  function handleReplySubmit(e, replyToComment = false, replyUserName = "") {
     e.preventDefault();
     if (!replyContent.trim()) return;
     const userReply = {
@@ -49,23 +50,29 @@ export default function FeedbackDetail() {
       username: data.currentUser.username,
       content: `${replyUserName} ` + replyContent,
       image: data.currentUser.image,
-    }
-    currentFeedback.comments.find(comment => comment.id === replyToComment).replies.push(userReply);
+    };
+    currentFeedback.comments.find((comment) => comment.id === replyToComment).replies.push(userReply);
     setData({ ...data });
-    setReply('');
-    setReplyContent('');
+    setReply("");
+    setReplyContent("");
   }
 
   return (
     <div className="feedback-detail-container">
       <div className="feedback-detail-header">
-        <a href="#/">Go back <img src="/images/right-arrow.svg" alt="" /></a>
+        <a href="#/">
+          Go back <img src="/images/right-arrow.svg" />
+        </a>
         <a href={`#/edit-feedback/${currentFeedback.id}`}>Edit Feedback</a>
       </div>
       <div className="card-info">
         <div className="card-content">
           <div className="card-header">
-            {screenSize >= 768 && <span>{currentFeedback.upvotes} <img src="/images/up-arrow.svg" alt="" /></span>}
+            {screenSize >= 768 && (
+              <span>
+                {currentFeedback.upvotes} <img src="/images/up-arrow.svg" />
+              </span>
+            )}
             <div>
               <h4>{currentFeedback.title}</h4>
               <p>{currentFeedback.description}</p>
@@ -73,11 +80,16 @@ export default function FeedbackDetail() {
             </div>
           </div>
           <div className="card-footer">
-            {screenSize >= 320 && screenSize < 768 && <span>{currentFeedback.upvotes} <img src="/images/up-arrow.svg" alt="" /></span>}
-            <span className="bg">{currentFeedback.comments?.length} <img src="/images/comment.svg" alt="" /></span>
+            {screenSize < 768 && (
+              <span>
+                {currentFeedback.upvotes} <img src="/images/up-arrow.svg" />
+              </span>
+            )}
+            <span className="bg">
+              {currentFeedback.comments?.length} <img src="/images/comment.svg" />
+            </span>
           </div>
         </div>
-
       </div>
       <div className="feedback-comments">
         <h3>{currentFeedback.comments?.length} Comments</h3>
@@ -86,7 +98,7 @@ export default function FeedbackDetail() {
             <div className="comments-area">
               <div className="comment">
                 <div className="comment-header">
-                  <img src={comment.image || `https://ui-avatars.com/api/?name=${comment.author.replace(' ', '+') || comment.name}`} alt="" />
+                  <img src={comment.image || `https://ui-avatars.com/api/?name=${comment.author.replace(" ", "+") || comment.name}`} alt="" />
                   <div>
                     <h4>{comment.author || comment.name}</h4>
                     <span>{comment.username}</span>
@@ -99,13 +111,13 @@ export default function FeedbackDetail() {
 
             {reply === comment.id && (
               <form onSubmit={(e) => handleReplySubmit(e, comment.id, comment.username)} autoComplete="off" className="reply-form">
-                <textarea name="userReply" id="" value={replyContent} onChange={(e) => setReplyContent(e.target.value)} className="text-area"></textarea>
+                <textarea name="userReply" value={replyContent} onChange={(e) => setReplyContent(e.target.value)} className="text-area"></textarea>
                 <button>Send</button>
               </form>
             )}
-            {comment.replies?.length > 0 && (
+            {comment.replies.length > 0 && (
               <div className="replies-section">
-                {comment.replies.map(x => (
+                {comment.replies.map((x) => (
                   <Fragment key={x.id}>
                     <div className="reply">
                       <div className="comment-header">
@@ -137,7 +149,7 @@ export default function FeedbackDetail() {
       <div className="add-comment">
         <h4>Add Comment</h4>
         <form onSubmit={handleSubmit} autoComplete="off">
-          <textarea name="userComment" id="" value={newComment} onChange={(e) => setNewComment(e.target.value)} placeholder="type your comment here" maxLength={250}></textarea>
+          <textarea name="userComment" value={newComment} onChange={(e) => setNewComment(e.target.value)} placeholder="type your comment here" maxLength={250}></textarea>
           <div className="add-comment-footer">
             <span>{250 - newComment.length} Characters left</span>
             <button>Post Comment</button>
